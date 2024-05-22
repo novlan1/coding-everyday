5052b89cd7a876a1d5eb0d81d53c5f24ea8a241f
novlan1 <1576271227@qq.com>
2024-05-22 09:51:42 +0800

    feat: add 情绪文章

--- article/go语言/1.helloworld.go
+++ article/go语言/1.helloworld.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
+
+	var a, b = int(5), uint(6)
+	var p uintptr = 0x12345678
+	fmt.Println("signed integer a's length is", unsafe.Sizeof(a))   // 8
+	fmt.Println("unsigned integer b's length is", unsafe.Sizeof(b)) // 8
+	fmt.Println("uintptr's length is", unsafe.Sizeof(p))            // 8
+
 	fmt.Println("hello world")
 }
 
